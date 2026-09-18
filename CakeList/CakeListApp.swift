20f46c3aacffd350c76154ed9791103b4041d754
Shikha Jain <shikha.singla@toolboxmarketing.com>
2026-09-18 10:04:56 +0100

    Initial Commit

---
+++ CakeList/CakeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CakeListApp.swift
+//  CakeList
+//
+//  Created by shikha on 18/09/26.
+//
+
+import SwiftUI
+
+@main
+struct CakeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
